e80ba99c14bb60fa9c677b6e123ee5c3687acdd2
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2020-12-29 21:41:10 -0400

    insert filter grid and two way data binding

--- ProAgil-App/src/app/events/events.component.ts
+++ ProAgil-App/src/app/events/events.component.ts
@@ -4,25 +4,54 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
 
-  events: any;
-  constructor(private http: HttpClient) { }
+  _filterList: string;
+  get filterList(): string {
+    return this._filterList;
+  }
+  
+  set filterList(value: string) {
+    this._filterList = value;
+    this.eventFiltered = this.filterList
+      ? this.filterEvents(this.filterList)
+      : this.events;
+  }
+
+  eventFiltered: any = [];
+  events: any = {};
+  imageWidth = 50;
+  imageMarge = 2;
+  showImage = false;
+
+  
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getEvents();
   }
 
-  getEvents(){
-    this.http.get("http://localhost:5000/events").subscribe(
-      response => { 
-        this.events = response 
-      }, error => {
-        console.log(error)
+  filterEvents(filterFor: string): any {
+    filterFor = filterFor.toLocaleLowerCase();
+    return this.events.filter(
+      events => events.theme.toLocaleLowerCase().indexOf(filterFor) !== -1
+      );
+  }
+  
+  alternateImage() {
+    this.showImage = !this.showImage;
+  }
+
+  getEvents() {
+    this.http.get('http://localhost:5000/events').subscribe(
+      (response) => {
+        this.events = response;
+      },
+      (error) => {
+        console.log(error);
       }
     );
   }
-
 }
