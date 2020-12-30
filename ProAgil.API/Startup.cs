bf6250c4092b4ceb3a6359fc5108643416744353
Allison Sousa Bahls <allison_sousa_bahls@hotmail.com>
2020-12-29 21:40:18 -0400

    Add images and file static

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -42,7 +42,7 @@ namespace ProAgil.API
 
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseAuthorization();
